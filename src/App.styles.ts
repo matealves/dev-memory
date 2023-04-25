557ec975d6e9e34b5104690dddf88e437687e18b
Mateus Alves <contatomateusalves@hotmail.com>
2023-04-24 21:02:52 -0300

    Update styles ⚙️

---
+++ src/App.styles.ts
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap");
+
+  //   font-family: Poppins, sans-serif;
+  width: 100%;
+  max-width: 750px;
+  margin: auto;
+  display: flex;
+  padding: 50px 0;
+
+  @media (max-width: 750px) {
+    flex-direction: column;
+  }
+`;
+
+export const Info = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: auto;
+
+  @media (max-width: 750px) {
+    margin-bottom: 50px;
+    align-items: center;
+  }
+`;
+
+export const LogoLink = styled.a`
+  display: block;
+`;
+
+export const InfoArea = styled.div`
+  width: 100%;
+  margin: 10px 0;
+
+  @media (max-width: 750px) {
+    display: flex;
+    justify-content: space-around;
+    text-align: center;
+  }
+`;
+
+export const GridArea = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+
+  @media (max-width: 750px) {
+    justify-content: center;
+    margin: 0 30px;
+  }
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as C from "./App.styles";
+import logoImage from "./assets/devmemory_logo.png";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <C.Container>
+      <C.Info>
+        <C.LogoLink>
+          <img src={logoImage} alt="" width="200" />
+        </C.LogoLink>
+
+        <C.InfoArea>...</C.InfoArea>
+
+        <button>Reiniciar</button>
+      </C.Info>
+      <C.GridArea>...</C.GridArea>
+    </C.Container>
+  );
 }
 
-export default App
+export default App;
